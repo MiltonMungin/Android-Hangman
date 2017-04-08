5afb06d78b265cbafbe7a87f2bb457fe3820c5c0
miltonm <mmungin1@student.gsu.edu>
2017-04-07 21:16:29 -0400

    Add category activity

---
+++ app/src/main/java/edu/gsu/student/hangman/MainActivity.java
@@ -0,0 +1,12 @@
+package edu.gsu.student.hangman;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
